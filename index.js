2f78c89eb95fcadccd8d12ab634202da26dd26a2
Sam Power <sam.g.power@gmail.com>
2019-03-06 19:13:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code in this file!
 function scuberGreetingForFeet (feet) {
-  if (feet =< 400)
-     return ""
+  if (feet =< 400) {
+     return "gives customers a free sample if the ride is less than or equal to 400 feet"
   }
   else 
 }
