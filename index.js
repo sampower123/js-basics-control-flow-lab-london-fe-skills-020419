c59160d4f56ce0b6cbf61cb4d2b646df9f92403f
Sam Power <sam.g.power@gmail.com>
2019-03-06 19:14:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code in this file!
 function scuberGreetingForFeet (feet) {
   if (feet <= 400) {
-     return "his one is on me!";
+     return "This one is on me!";
   }
   else{
      return "";
