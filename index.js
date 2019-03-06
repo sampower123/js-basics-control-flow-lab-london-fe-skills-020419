6a4d13dcc066a9e854c5d76aa25356b3b001d4f2
Sam Power <sam.g.power@gmail.com>
2019-03-06 19:15:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ function scuberGreetingForFeet (feet) {
   if (feet <= 400) {
      return "This one is on me!";
   }
-  else if (feet > 2000){
+  else if (feet > 2000) {
      return "I will gladly take your thirty bucks.";
+  }
+  else if (feet > 2500) {
+     return "No can do.";
   }
 }
